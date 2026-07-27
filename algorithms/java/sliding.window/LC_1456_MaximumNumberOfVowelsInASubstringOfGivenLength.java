7729846be0eccfcf889602ff095f4ed725a7282a
Ilian Zapryanov <ilian.iliev200@gmail.com>
2026-07-27 16:25:36 +0300

    add solution to 1456. Maximum Number of Vowels in a Substring of Given Length

---
+++ algorithms/java/sliding.window/LC_1456_MaximumNumberOfVowelsInASubstringOfGivenLength.java
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/maximum-number-of-vowels-in-a-substring-of-given-length/description
+
+package sliding.window;
+
+public class LC_1456_MaximumNumberOfVowelsInASubstringOfGivenLength {
+    public int maxVowels(String s, int k) {
+        Set<Character> vowels = Set.of('a', 'e', 'i', 'o', 'u');
+
+        int maxCount = -1;
+        int currLength = 0;
+        int vowelsCount = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (vowels.contains(s.charAt(i))) {
+                vowelsCount++;
+            }
+            currLength++;
+
+            if (currLength > k) {
+                currLength--;
+                vowelsCount -= vowels.contains(s.charAt(i - k)) ? 1 : 0;
+            }
+
+            if (currLength == k) {
+                maxCount = Math.max(maxCount, vowelsCount);
+            }
+        }
+
+        return maxCount;
+    }
+}
