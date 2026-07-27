310c682656a187be20d456802443c4d7bd919c05
Ilian Zapryanov <ilian.iliev200@gmail.com>
2026-07-27 16:43:45 +0300

    fix imports for 1456. Maximum Number of Vowels in a Substring of Given Length

--- algorithms/java/sliding.window/LC_1456_MaximumNumberOfVowelsInASubstringOfGivenLength.java
+++ algorithms/java/sliding.window/LC_1456_MaximumNumberOfVowelsInASubstringOfGivenLength.java
@@ -2,6 +2,8 @@
 
 package sliding.window;
 
+import java.util.Set;
+
 public class LC_1456_MaximumNumberOfVowelsInASubstringOfGivenLength {
     public int maxVowels(String s, int k) {
         Set<Character> vowels = Set.of('a', 'e', 'i', 'o', 'u');
