dd70c7b3d1e327c2d160a2bdfdda3da62ff641dc
Ilian Zapryanov <ilian.iliev200@gmail.com>
2026-07-30 13:34:54 +0300

    add solution to 1929. Concatenation of Array

---
+++ algorithms/java/arrays/LC_1929_ConcatenationOfArray.java
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/concatenation-of-array/description/
+
+package arrays;
+
+public class LC_1929_ConcatenationOfArray {
+    public int[] getConcatenation(int[] nums) {
+        int[] ans = new int[nums.length * 2];
+
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = nums[i];
+            ans[i + nums.length] = nums[i];
+        }
+
+        return ans;
+    }
+}
