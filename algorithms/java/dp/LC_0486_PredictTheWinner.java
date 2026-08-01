f545cec181d5182dc4e7ff793b8f53fe857cdc17
Ilian Zapryanov <ilian.iliev200@gmail.com>
2026-08-01 16:56:18 +0300

    add solution to 486. Predict the Winner

---
+++ algorithms/java/dp/LC_0486_PredictTheWinner.java
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/predict-the-winner/description
+
+package dp;
+
+public class LC_0486_PredictTheWinner {
+    public boolean predictTheWinner(int[] nums) {
+        int[][] dp = new int[nums.length][nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            dp[i][i] = nums[i];
+        }
+
+        for (int i = nums.length - 2; i >= 0; i--) {
+            for (int j = i + 1; j < nums.length; j++) {
+                dp[i][j] = Math.max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1]);
+            }
+        }
+
+        return dp[0][nums.length - 1] >= 0;
+    }
+}
