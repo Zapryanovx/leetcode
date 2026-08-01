9482d0e82cb4623042226f097cdeac07ba908b39
Ilian Zapryanov <ilian.iliev200@gmail.com>
2026-08-01 16:23:32 +0300

    fix 208. Implement Trie (Prefix Tree)

--- algorithms/java/trie/LC_0208_ImplementTriePrefixTree.java
+++ algorithms/java/trie/LC_0208_ImplementTriePrefixTree.java
@@ -3,44 +3,46 @@
 package trie;
 
 public class LC_0208_ImplementTriePrefixTree {
-    private Trie[] children;
-    private boolean isEnd;
+    static class Trie {
+        private Trie[] children;
+        private boolean isEnd;
 
-    public Trie() {
-        children = new Trie[26];
-        isEnd = false;
-    }
-
-    public void insert(String word) {
-        Trie node = this;
-        for (char c: word.toCharArray()) {
-            int i = c - 'a';
-            if (node.children[i] == null) {
-                node.children[i] = new Trie();
-            }
-            node = node.children[i];
+        public Trie() {
+            children = new Trie[26];
+            isEnd = false;
         }
-        node.isEnd = true;
-    }
 
-    public boolean search(String word) {
-        Trie node = find(word);
-        return node != null && node.isEnd;
-    }
-
-    public boolean startsWith(String prefix) {
-        return find(prefix) != null;
-    }
-
-    private Trie find(String s) {
-        Trie node = this;
-        for (char c: s.toCharArray()) {
-            int i = c - 'a';
-            if (node.children[i] == null) {
-                return null;
+        public void insert(String word) {
+            Trie node = this;
+            for (char c: word.toCharArray()) {
+                int i = c - 'a';
+                if (node.children[i] == null) {
+                    node.children[i] = new Trie();
+                }
+                node = node.children[i];
             }
-            node = node.children[i];
+            node.isEnd = true;
         }
-        return node;
+
+        public boolean search(String word) {
+            Trie node = find(word);
+            return node != null && node.isEnd;
+        }
+
+        public boolean startsWith(String prefix) {
+            return find(prefix) != null;
+        }
+
+        private Trie find(String s) {
+            Trie node = this;
+            for (char c: s.toCharArray()) {
+                int i = c - 'a';
+                if (node.children[i] == null) {
+                    return null;
+                }
+                node = node.children[i];
+            }
+            return node;
+        }
     }
 }
