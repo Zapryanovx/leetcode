c8904813a449b4a4b23f23358229c7dc49e189d0
Ilian Zapryanov <ilian.iliev200@gmail.com>
2026-08-02 03:11:57 +0300

    add solution to 190. Reverse Bits

---
+++ algorithms/java/bit/manipulation/LC_0190_ReverseBits.java
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/reverse-bits/description
+
+package bit.manipulation;
+
+public class LC_0190_ReverseBits {
+    public int reverseBits(int n) {
+        int res = 0;
+
+        for (int i = 0; i < 32; i++) {
+            res <<= 1;
+            res |= (n & 1);
+            n >>>= 1;
+        }
+
+        return res;
+    }
+}
