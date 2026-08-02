73c1db7c2b23b04dcbd6e4bb309c2a8596829bbd
Ilian Zapryanov <ilian.iliev200@gmail.com>
2026-08-02 03:12:15 +0300

    add solution to 877. Stone Game

---
+++ algorithms/java/dp/LC_0877_StoneGame.java
@@ -0,0 +1,21 @@
+//
+
+package dp;
+
+public class LC_0877_StoneGame {
+    public boolean stoneGame(int[] piles) {
+        int[][] dp = new int[piles.length][piles.length];
+
+        for (int i = 0; i < piles.length; i++) {
+            dp[i][i] = piles[i];
+        }
+
+        for (int i = piles.length - 2; i >= 0; i--) {
+            for (int j = i + 1; j < piles.length; j++) {
+                dp[i][j] = Math.max(piles[i] - dp[i + 1][j], piles[j] - dp[i][j - 1]);
+            }
+        }
+
+        return dp[0][piles.length - 1] >= 0;
+    }
+}
