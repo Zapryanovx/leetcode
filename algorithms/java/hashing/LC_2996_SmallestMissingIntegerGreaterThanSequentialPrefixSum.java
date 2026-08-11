b19b2d6715b06386813ea61ef7e40b3b17a9f544
Ilian Zapryanov <ilian.iliev200@gmail.com>
2026-08-12 01:16:54 +0300

    add solution to 2996. Smallest Missing Integer Greater Than Sequential Prefix Sum

---
+++ algorithms/java/hashing/LC_2996_SmallestMissingIntegerGreaterThanSequentialPrefixSum.java
@@ -0,0 +1,29 @@
+package hashing;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LC_2996_SmallestMissingIntegerGreaterThanSequentialPrefixSum {
+    public int missingInteger(int[] nums) {
+        int sum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1] + 1) {
+                sum += nums[i];
+            } else {
+                break;
+            }
+        }
+
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        while (set.contains(sum)) {
+            sum++;
+        }
+
+        return sum;
+    }
+}
